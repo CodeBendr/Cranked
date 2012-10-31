533e8c4e31016e38d02a75d0f181c5b795fab853
Skrizy <youngskrilla26@hotmail.com>
2012-10-31 00:30:50 +0000

    Cranked beta finished

--- Script/Crank.js
+++ Script/Crank.js
@@ -21,57 +21,138 @@
   
     //public constants
     Crank.PACKAGE = "com.Assignment.Foundation.Crank";
-
-	//animate logo
-   Crank.AnimateTopMargin = function(opt){
-	   
-	//create defaults
-	defaults = {speed: 1000,value:90,callback:function(){console.log("Complete");}}
+	Crank.COUNTRY = "";
+	Crank.NUMBER_LENGTH = 0;
 	
-	//handle jQuery animation method with options
-	$("#"+opt.id).animate({marginTop:opt.value},opt.speed,function(){opt.callback()});
+	Crank.isNumber =  function(n) {
+		
+     return !isNaN(parseFloat(n)) && isFinite(n);
+	 
+    }
 	
-	//check for default options
-	opt = typeof opt == 'object' ? $.extend(defaults, opt) : opt = defaults;
-	
+	Crank.Starter = function(opt){
+		
+		//create defaults
+	    defaults = {speed:900}
+		
+		 $("#"+opt.container).animate({"marginTop":30},1200,function(){
+		 
+		  $("#"+opt.welcome).fadeIn(opt.speed);
+		  $("#"+opt.line).fadeIn(opt.speed)
+		  $("#"+opt.button).show();
+		  $("#"+opt.button).animate({"marginTop":0},opt.speed);
+		 
+		 });
+		 
+		 //check for default options
+	     opt = typeof opt == 'object' ? $.extend(defaults, opt) : opt = defaults;
+		
+		}
+		
+	Crank.StarterButton = function(){
+		
+		
+		
+		}
+		
+	function geocodingSettings(){
+		
+		
+		 $("#crank_logo").animate({"marginLeft":0},700,function(){
+			 
+			  $("#crank_logo_container").animate({"marginTop":-260},700,function(){
+				 
+				 $("#crank_logo").hide();
+				 $("#crank_logo_small").show(function(){
+					 
+					 $("#crank_logo_container").animate({"marginTop":-130})
+					 $("#crank_logo_small").animate({"marginTop":140},function(){
+						 
+						 $("#crank_welcome").hide();
+						  $("#crank_btnStarted").animate({"marginTop":-87});
+						  
+				$("#crank_btnStarted").animate({"marginTop":-90})
+				$("#crank_txtFieldSet_country").fadeIn();
+				$("#crank_txtFieldSet_zipcode").show();
+				$("#crank_txtFieldSet_zipcode").animate({"marginTop":0})
+				$("#crank_txtChooseCountry").hide();
+				$("#crank_number").fadeIn();
+				$("#number").focus();
+				$("#crank_btnVerify").fadeIn();
+					
+						   
+						 })
+					 
+					 });
+				
+				 
+				 });
+			 
+			 })
+		
+		
+		}
+		
+	function manualLocation(){
+		
+		Crank.zipCode();
+		
+		 $("#crank_logo").animate({"marginLeft":0},700,function(){
+			 
+			  $("#crank_logo_container").animate({"marginTop":-260},700,function(){
+				 
+				 $("#crank_logo").hide();
+				 $("#crank_logo_small").show(function(){
+					 
+					 $("#crank_logo_container").animate({"marginTop":-130})
+					 $("#crank_logo_small").animate({"marginTop":140},function(){
+						 
+						 $("#crank_welcome").hide();
+						  $("#crank_txtChooseCountry").fadeIn(); 
+						  $("#crank_btnStarted").animate({"marginTop":-87})
+						   $(".crank_btnCountry").fadeIn();
+						   
+						 })
+					 
+					 });
+				
+				 
+				 });
+			 
+			 })
+		
+		}
+		
+	Crank.zipCode = function(){
+		
+		countries = [{country:"GHANA",zipcode:233},{country:"NIGERIA",zipcode:2},{country:"UK",zipcode:44}];
+		
+		$(".crank_text_spacing").click(function(){
+			
+			    $("#crank_btnStarted").animate({"marginTop":-162})
+				$("#crank_txtFieldSet_country").fadeIn();
+				$("#crank_txtFieldSet_text").text($(this).text());
+				if($(this).text() == "GHANA"){$("#crank_txtFieldSet_title_sub").text("+233"); Crank.NUMBER_LENGTH = 9;}
+				if($(this).text() == "NIGERIA"){$("#crank_txtFieldSet_title_sub").text("+2"); Crank.NUMBER_LENGTH = 12;}
+				if($(this).text() == "UK"){$("#crank_txtFieldSet_title_sub").text("+44"); Crank.NUMBER_LENGTH = 10}
+				$("#crank_txtFieldSet_zipcode").show();
+				$("#crank_txtFieldSet_zipcode").animate({"marginTop":0})
+				$("#crank_txtChooseCountry").hide();
+				$("#crank_number").fadeIn();
+				$("#number").focus();
+				$("#crank_btnVerify").fadeIn();
+			
+			
+		})
 	}
 	
 	var latitude;
 	var longitude;
 	
-	  function getCountry(results)
-    {
-        for (var i = 0; i < results[0].address_components.length; i++)
-        {
-        var shortname = results[0].address_components[i].short_name;
-        var longname = results[0].address_components[i].long_name;
-        var type = results[0].address_components[i].types;
-        if (type.indexOf("country") != -1)
-        {
-            if (!isNullOrWhitespace(shortname))
-            {
-                alert(shortname);
-            }
-            else
-            {
-                alert(longname);
-            }
-        }
-    }
-
-}
-
-function isNullOrWhitespace(text) {
-    if (text == null) {
-        return true;
-    }
-    return text.replace(/\s/gi, '').length < 1;
-}
-	
-	//get the user country
+	 //get the user country
 	 Crank.UserCountry = function(){
 		 
-		 if (navigator.geolocation) {
+		if (navigator.geolocation) {
 			 
        console.log('Geolocation is supported!');
 	   
@@ -81,47 +162,98 @@ function isNullOrWhitespace(text) {
        startPos = position;
        latitude =  startPos.coords.latitude;
       longitude = startPos.coords.longitude;
-	  url = "http://maps.googleapis.com/maps/api/geocode/json?latlng="+latitude+","+longitude+"&sensor=false";
-	  alert(longitude);
-	  $.getJSON(url,
-        function(data){
-			
-			alert(data);
-			
-          $.each(data.items, function(i,item){
-			  
-            $("<img/>").attr("src", item.media.m).appendTo("#images");
-			
-            if ( i == 3 ) return false;
-			
-           });
-        });
+	//  url = "http://maps.googleapis.com/maps/api/geocode/json?latlng="+latitude+","+longitude+"&sensor=false";
+	url  = "https://api.foursquare.com/v2/venues/search?ll="+latitude+","+longitude+"&oauth_token=3IYMA1D0J2OFDMORTCOIEWZMLUGW2HCRTMGWSXDQN5RQBGR2&v=20120407";
+
+	$.ajax({
+    type: "GET",
+    url: url,
+	async: false,
+    dataType: "jsonp",
+	jsonpCallback: 'jsonCallback',
+ contentType: "application/json",
+    success: function(json) {
+
+ Crank.COUNTRY = String(json.response.venues[0].location.country).toUpperCase();
+ 
+  if(Crank.COUNTRY == "GHANA"){
 	  
-	  /* geocoder = new google.maps.Geocoder();
-	  country = geocoder.getLocations(latitude+","+longitude, function(results,status){
+	  $("#crank_txtFieldSet_title_sub").text("+233"); 
+	  console.log(Crank.COUNTRY);
+	  geocodingSettings();
+	  $("#crank_txtFieldSet_text").text(Crank.COUNTRY);
+	  
+	  } else if(Crank.COUNTRY == "NIGERIA"){
 		  
-		  if (status == google.maps.GeocoderStatus.OK) {
+		  $("#crank_txtFieldSet_title_sub").text("+2")
+		  
+		  }else if(Crank.COUNTRY == "UK"){
 			  
-                if (results[0]) {
-					
-                    var loc = getCountry(results);
-                }
-		     }
-			 
-		  });*/
+			  $("#crank_txtFieldSet_title_sub").text("+44")
+			  
+			  
+			  }else{
+			  
+			  manualLocation();}
+ // if(Crank.COUNTRY == "UK"){$("#crank_txtFieldSet_title_sub").text("+44")}else{manualLocation();}
+ 
+
+
+    },
+    error: function(e) {
+		
+       console.log("error");
+	   manualLocation();
+    
+		}
+})
 	
 	   }, function(error) {
 		   
-		   
+		   console.log('Error Getting location');
+		   manualLocation();
 		   
 		   })
          
 		 }else{
 			 
         console.log('Geolocation is not supported for this Browser/OS version yet.');
+	    manualLocation();
   
       }
  }
+ 
+ 
+ Crank.CoverBox = function(opt){
+	
+     var coverbox = new com.lasagna.iComp.CoverBox();
+ 
+     coverbox.buttonStyle("#415762","#415762","#fff");
+	coverbox.CoverBoxStyles("url('Assets/coverBox.png')");
+	coverbox.closeButtonStyle("none","#fff","13px");
+	coverbox.titleStyle("#415762","#fff","16px");
+
+	coverbox.useCloseButton = true;
+	coverbox.useModalClose  = false;
+	coverbox.jQuery         = true;
+	coverbox.useButtons     = true;
+			
+	coverbox.Text = opt.text;//"Surname or First Name Not Found; Please Check Before Proceeding";
+	
+	coverbox.Title = opt.title//"Empty Fields Detected";
+	coverbox.Buttons("OK");
+    coverbox.add();
+	$("#body").css({"overflow":"hidden"});
+	//$(coverbox.jQueryID.box).hide();
+    coverbox.jQueryEvent("click",function(){
+		
+	$(coverbox.jQueryID.box).fadeOut("fast",function(){coverbox.destroy()});
+	$("#body").css({"overflow":"auto"});
+	$("#number").focus();
+		
+	}); 
+	
+}
 	
 	//return an object by using the document.getElementById native function   
 	Crank.o = function(id){
--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 <title>Let's Get Cranky</title>
 
 <link rel="stylesheet" type="text/css" href="Style/style.css">
+<link rel="stylesheet" type="text/css" href="Style/CoverBoxStyle.css">
 
 </head>
 
@@ -18,20 +19,129 @@
   <!--Background-->
   
   <!--Logo-->
+  <div class="crank_logo_container" id="crank_logo_container">
   <div class="crank_logo" id="crank_logo"></div>
+  <div class="crank_logo_small" id="crank_logo_small"></div>
+  </div>
   <!--Logo-->
+  
+  <!--clear-->
+  <div class="crank_clear"></div>
+  <!--clear-->
   
   <!--Main-->
   <div class="crank_main" id="crank_main">
   
   <!--welcome text-->
   <div class="crank_welcome" id="crank_welcome"></div>
+  <div class="crank_txtChooseCountry" id="crank_txtChooseCountry"></div>
+  <div class="crank_number" id="crank_number"></div>
   <div class="crank_welcome_line" id="crank_welcome_line"></div>
   <!--welcome text-->
+  
+  <!--Button Domain-->
+  <div class="crank_button_domain" id="crank_button_domain">
   
   <!--Button Started-->
   <div class="crank_btnStarted" id="crank_btnStarted"></div>
   <!--Button Started-->
+  
+  <!--Countries-->
+  <div style="margin-left:50px;">
+  
+  <div class="crank_btnCountry">
+  <div class="crank_text_spacing">GHANA</div>
+  <div class="crank_number" style="display:none">0</div>
+  </div>
+  
+   <div class="crank_btnCountry">
+  <div class="crank_text_spacing">NIGERIA</div>
+  <div class="crank_number" style="display:none">2</div>
+  </div>
+  
+  <div class="crank_btnCountry">
+  <div class="crank_text_spacing">UK</div>
+  <div class="crank_number" style="display:none">3</div>
+  </div>
+  
+  </div>
+  <!--Countries-->  
+  
+  <!--clear-->
+  <div class="crank_clear"></div>
+  <!--clear-->
+  
+  <!--Field Set-->
+  <div class="crank_txtFieldSet" id="crank_txtFieldSet_country">
+  
+  <div style="float:left">
+  <div class="crank_check"></div>
+  <div class="crank_txtFieldSet_title">country</div>
+  </div>
+
+  <div class="crank_txtFieldSet_text" id="crank_txtFieldSet_text">
+  </div>
+  
+  </div>
+  <!--Field Set-->
+  
+   <!--Field Set-->
+  <div class="crank_txtFieldSet" style="margin-top:20" id="crank_txtFieldSet_zipcode">
+  
+  <div style="float:left">
+  <div class="crank_txtFieldSet_title">zipcode</div>
+  <div class="crank_txtFieldSet_title_sub" id="crank_txtFieldSet_title_sub"></div>
+  </div>
+
+  <input type="text" name="number" id="number" value=""/>
+   <div class="crank_txtFieldSet_text" id="crank_txtFieldSet_number">
+  </div>
+  </div>
+  <!--Field Set-->
+  
+  
+   <!--button verify-->
+  <div class="crank_btnVerify" id="crank_btnVerify">
+  </div>
+  <!--button verify-->
+  
+  <!--Field Set-->
+  <div class="crank_txtFieldSet" style="margin-top:20px" id="crank_txtFieldSet_password">
+  
+  <div style="float:left">
+  <div class="crank_password"></div>
+  <div class="crank_txtFieldSet_title">password</div>
+  </div>
+
+  <input type="password" name="password" id="password" value=""/>
+  
+  </div>
+  <!--Field Set-->
+  
+  <!--Field Set-->
+  <div class="crank_txtFieldSet" id="crank_txtFieldSet_passwordMatch">
+  
+  <div style="float:left">
+  <div class="crank_password" style="margin-top:2px;"></div>
+  <div class="crank_txtFieldSet_title" style="margin-top:0px">password <br> [retype]</div>
+  </div>
+
+   <input type="password" name="passwordMatch" id="passwordMatch" value=""/>
+  
+  </div>
+  <!--Field Set-->
+  
+  <!--Button Proceed-->
+  <div class="crank_btnProceed" id="crank_btnProceed"></div>
+   <!--Button Proceed-->
+  
+  </div>
+    <!--Button Domain-->
+  
+ 
+  
+ 
+
    
   </div>
   <!--Main-->
@@ -39,9 +149,10 @@
   </div>
 <!--MasterLayer-->
   
-<script type="text/javascript" src="Script/jquery-1.4.3.min.js"></script>
+<script type="text/javascript" src="Script/jquery-1.8.2.js"></script>
 <script type="text/javascript" src="Script/jquery.easing.1.3.js"></script>
 <script type="text/javascript" src="Script/Crank.js"></script>
+<script type="text/javascript" src="Script/CoverBox.js"></script>
 
 
 <script type="text/javascript">
@@ -50,27 +161,83 @@
 	  
 	 jQuery.easing.def = "easeInOutQuart";
 	 
-	 Crank.AnimateTopMargin({id:"crank_logo",
-	                         value:30,
-							 speed:900,
-							 callback:function(){
-								 
-								 $("#crank_welcome").fadeIn(1000);
-								 $("#crank_welcome_line").fadeIn(1000)
-									 
-									 $("#crank_btnStarted").show();
-									 Crank.AnimateTopMargin({id:"crank_btnStarted",value:0});
-									 
-									}
-	                         })
+	Crank.Starter({container:"crank_logo_container",
+	               welcome:"crank_welcome",
+				   line:"crank_welcome_line",
+				   button:"crank_btnStarted"});
+				  
+	
+	$("#crank_btnProceed").click(function(){
+		
+		var password = $("#password").val();
+        var passwordMatch = $("#passwordMatch").val();
+		
+        if ((password.length < 6 ) && (password.length < 6) || (password.length > 6 ) && (password.length > 6)) {
+			
+         Crank.CoverBox({title:"Password Length",text:"Please make sure your password is 6 characters"});
+		  
+       }else if(password == passwordMatch){
+		   
+		    Crank.CoverBox({title:"Welcome To Crank",text:"You Are Officially Cranked"});
+		   
+		   }else{
+			   
+			 Crank.CoverBox({title:"Password Mismatch",text:"Your passwords don't match"});  
+			   
+			   }
+		   
+		})
+	
+	$("#crank_btnVerify").click(function(){
+		
+		if($("#number").val().length == 0 ){
+			
+		 Crank.CoverBox({title:"Number Field Empty",text:"Please enter your number"});
+			
+			}else if($("#number").val().charAt(0) == 0 ){
+			
+		 Crank.CoverBox({title:"Number With Leading Zero",text:"Please enter number without the leading zero"});
+			
+			}else if($("#number").val().length < Crank.NUMBER_LENGTH){
+		
+		 Crank.CoverBox({title:"Number Length Too Short",text:"Please Check the length of the number, make sure its " + Crank.NUMBER_LENGTH});
+						
+		  }else if($('#number').val() != ""){
+			  
+		var value = $('#number').val().replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+        var intRegex = /^\d+$/;
+        if(!intRegex.test(value)) {
+		Crank.CoverBox({title:"Invalid Number",text:"This is not a valid number"});
+         
+		}else{
+			
+			  
+			   $("#crank_btnVerify").fadeOut(function(){
+			  
+			  $("#password").focus();
+			  $("#crank_txtFieldSet_password").show();
+			  $("#crank_txtFieldSet_passwordMatch").show();
+			  $("#crank_txtFieldSet_password").animate({"marginTop":0});
+			  $("#crank_btnProceed").fadeIn();
+			  $("#number").hide();
+			  $("#crank_txtFieldSet_number").text($("#number").val());
+			  
+			  });
+			
+			
+			}
+		
+			  
+			  }
+		})
 	
 	$("#crank_btnStarted").click(function(){
 		
 		Crank.UserCountry();
 		
-		});
-    
-});
+		
+		  });
+    });
 
 </script>
 </body>
